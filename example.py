30ed8866281dd93ea408283e20d3838edc7bd728
gecko1990 <rafael_laso@hotmail.com>
2023-03-30 10:34:21 +0200

    restore function subtract; fixes #1

--- example.py
+++ example.py
@@ -2,6 +2,7 @@ def add(a, b):
     return a + b
 
 #esto deberia ser un comentario
+#esto para cerrar el issue
 def test_add():
     assert add(2, 3) == 5
     assert add('space', 'ship') == 'spaceship'
